7193c9993fb2280e8c6b1ed0f33aec082b5f2862
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-12-02 18:35:35 +0000

    Initial commit

---
+++ taskmanager/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env # noqa (no quality assurance)
+
+
+app = Flask(__name__) # default flask name module
+# app configuration variables from env.py file
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+
+from taskmanager import routes
